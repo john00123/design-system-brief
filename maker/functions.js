9d8dcabb1050228bb2af2df8db76ba29c4cf5446
John NTNX <john.torres@nutanix.com>
2021-02-17 20:22:21 -0800

    maker changes

--- maker/functions.js
+++ maker/functions.js
@@ -10,13 +10,13 @@ var output = d.getFullYear() + '/' +
 
 var section = `<!-- Section -->
 <table class='section' cellpadding="0" style="border-collapse:collapse;border-spacing:0;width:100%;">
-  <td class="outlook-row-container bottom_3 top_3 left_6_3 right_6_3" style="padding-bottom: 24px; padding-top: 24px; padding-left: 48px; padding-right: 48px; 1px solid red;">
+  <td class="outlook-row-container bottom_3 top_3 left_6_3 right_6_3" style="padding-bottom: 24px; padding-top: 24px; padding-left: 48px; padding-right: 48px;">
 
     <!-- Title -->
     <table cellpadding="0"  style="border-collapse:collapse;border-spacing:0;width:100%;">
       <td>
         <h3 contenteditable="true" style="font-size: 20px; line-height: 26px; color: #22272E; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; font-weight: 500; margin: 0;mso-line-height-rule:exactly;">
-          Title
+          Figma Migration
         </h3>
       </td>
     </table>
@@ -25,18 +25,18 @@ var section = `<!-- Section -->
     <table cellpadding="0" style="border-collapse:collapse;border-spacing:0;width:100%;">
       <td style="padding-top: 8px;">
         <p contenteditable="true" style="font-size: 18px; line-height: 28px; font-weight:300; ; color: #4D5969; font-family: 'nutanix_soft',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; margin: 0 !important;">
-          Paragraph text
+          We will be releasing a library for Figma on the week of Feb 15 - 19. All new and existing projects under our corporate account will have immediate access to them. The library requires no set-up. Things like fonts are also automatically taken care of through the platform. No need to be looking for the font files and color files anymore.
         </p>
       </td>
     </table>
-    <!-- End of Paragraph -->
+    <!-- End of Prargraph -->
   </td>
 </table>
 <!-- End of Section -->`
 
 var paragraph =`
 <!-- Paragraph -->
-    <br><p contenteditable="true" style="font-size: 18px; line-height: 28px; font-weight:300; ; color: #4D5969; font-family: 'nutanix_soft',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; margin: 0 !important;">
+    <p contenteditable="true" style="font-size: 18px; line-height: 28px; padding-top:20px; font-weight:300; ; color: #4D5969; font-family: 'nutanix_soft',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; margin: 0 !important;">
       Paragraph text
     </p>
 <!-- End of Paragraph -->
@@ -66,25 +66,26 @@ $(document).ready(function(){
     $('.modal_bg').show();
   });
 
-  $('.copy-btn').click(function(){
-    var copyText = $('.codeInput').val();
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
+  var d = new Date();
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  var n = month[d.getMonth()];
+  var year = d.getFullYear();
 
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.val());
-  });
 
-  function myFunction() {
-    var copyText = document.getElementById("myInput");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
-  }
+  $('.date').text(n +' '+year);
 
   $('.change').click(function(){
     $('.modal_bg').hide();
